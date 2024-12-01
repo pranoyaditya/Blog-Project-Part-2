4b95036074a1d9ead802cbb44bd178d99f1ea879
pranoyaditya <pranoyaditya@gmail.com>
2024-12-01 21:14:43 +0600

    added login page, userLogin form, created user_login view.

--- simple_blog_project/author/views.py
+++ simple_blog_project/author/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,UserLoginForm
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
-def add_author(request):
-    if request.method == 'POST':
-        author_form = AuthorForm(request.POST)
-        if author_form.is_valid():
-            author_form.save()
-            return redirect('add_author')
-    else:
-        author_form = AuthorForm()
-    return render(request, 'author/add_author.html', {'form' : author_form})
-
 def register(request):
     form = RegisterForm()
     if request.method == 'POST':
@@ -19,4 +10,17 @@ def register(request):
         if form.is_valid():
             form.save()
             return redirect('register')
-    return render(request, 'author/register.html', {'form' : form})
+    return render(request, 'author/register.html', {'form' : form, 'type': 'Register'})
+
+def user_login(request):
+    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            userName = form.cleaned_data['username']
+            userPass = form.cleaned_data['password']
+            user = authenticate(username = userName, password = userPass)
+            if user:
+                login(request,user)
+                return redirect('profile')
+    return render(request, 'author/register.html', {'form' : form, 'type': 'Login'})
