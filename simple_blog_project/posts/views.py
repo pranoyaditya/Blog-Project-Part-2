456eb9929e04ffcfa554f9e1d2e61fa254436275
pranoyaditya <pranoyaditya@gmail.com>
2024-12-02 00:36:31 +0600

    updated the add post section

--- simple_blog_project/posts/views.py
+++ simple_blog_project/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 # Create your views here.
@@ -7,12 +8,14 @@ def add_post(request):
     if request.method == 'POST':
         post_form = PostForm(request.POST)
         if post_form.is_valid():
+            post_form.instance.author = request.user
             post_form.save()
-            return redirect('add_post')
+            return redirect('homePage')
     else:
         post_form = PostForm()
     return render(request, 'posts/postPage.html', {'form': post_form})
 
+@login_required
 def edit_post(request,id):
     post = Post.objects.get(pk=id)
     post_form = PostForm(instance=post)
@@ -23,6 +26,7 @@ def edit_post(request,id):
             return redirect('homePage')
     return render(request, 'posts/postPage.html', {'form': post_form})
 
+@login_required
 def delete_post(reuqest, id):
     post = Post.objects.get(pk = id)
     post.delete()
