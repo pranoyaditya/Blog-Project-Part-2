df465e30f47098961ac3a4114c265cde2fd2e12b
pranoyaditya <pranoyaditya@gmail.com>
2024-12-02 22:01:12 +0600

    added slug field to filter posts depending on categories

--- simple_blog_project/simple_blog_project/urls.py
+++ simple_blog_project/simple_blog_project/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='homePage'),
+    path('category/<slug:category_slug>/', views.home, name='category_wise_post'),
     path('author/', include('author.urls')),
     path('posts/', include('posts.urls')),
     path('categories/', include('categories.urls')),
